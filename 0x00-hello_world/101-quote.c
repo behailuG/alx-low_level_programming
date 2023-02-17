aaf11452748d4ccb2ea4158131f2f6fd672ba0bf
behailuG <barch.mami@gmail.com>
2023-02-17 11:16:27 +0300

    quete

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
  * main - dgg
  * Return: 1 (Success)
  */
-int main(viud)
+int main(void)
 {
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19",stderr);
 return (1);
 }
