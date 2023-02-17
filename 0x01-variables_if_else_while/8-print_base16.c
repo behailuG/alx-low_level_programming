43b67954bc0ecef09bb4129a653ca8f5ac2acfb5
behailuG <barch.mami@gmail.com>
2023-02-17 14:07:50 -0800

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,7 +9,7 @@ int i;
 char letter;
 for (i = 0; i < 10; i++)
 putchar(i % 10 + '0');
-for (letter = 'a'; letter < 'f'; letter++)
+for (letter = 'a'; letter <= 'f'; letter++)
 putchar(letter);
 putchar('\n');
 return (0);
