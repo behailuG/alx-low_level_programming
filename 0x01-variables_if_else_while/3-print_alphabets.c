2d8f2276a56f39a848af6470f837dbabfddc11ae
behailuG <barch.mami@gmail.com>
2023-02-17 13:02:33 -0800

    lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - lower and upper case
+ *Return: Always 0.
+ */
+int main(void)
+{
+char lower[26]="abcdefghijklmnopqrstuvwxyz";
+char upper[26]="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(lower[i]);
+putchar(upper[i]);
+}
+putchar('\n');
+}
