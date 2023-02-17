e695f9f8302f78c70dd844e2e9c1693971597a89
behailuG <barch.mami@gmail.com>
2023-02-17 15:13:27 -0800

    revers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,12 +5,14 @@
  */
 int main(void)
 {
-int i,j;
+int i, j;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
 if (i == j)
+continue;
+if (i < j)
 continue;
 putchar((i % 10) + '0');
 putchar((j % 10) + '0');
