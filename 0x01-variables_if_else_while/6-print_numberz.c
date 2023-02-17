413af090ff78289af59f1f62be0b4124857de2ab
behailuG <barch.mami@gmail.com>
2023-02-17 13:41:11 -0800

    single digit without char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - single digit without char
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i);
+}
+putchar("\n");
+return (0);
+}
