6d5fa0f0dde6e838610a1d2c87e42254fff70fd5
behailuG <barch.mami@gmail.com>
2023-02-21 13:51:49 -0800

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,6 +5,22 @@
  */
 void print_to_98(int n)
 {
+	int i, rem, len = 0, number;
+	char str[5]="abcde";
+    number = n;
+    while (number != 0)
+    {
+        len++;
+        number /= 10;
+    }
+    for (i = 0; i < len; i++)
+    {
+        rem = n % 10;
+        n = n / 10;
+        str[len - (i + 1)] = rem + '0';
+    }
+    str[len] = '\0';
+
 	if (n <= 98)
 	{
 		for (; n <= 98; n++)
@@ -17,6 +33,7 @@ void print_to_98(int n)
 			}
 			else
 			{
+
 				_putchar(n + '0');
 			}
 		}
