be273ca8161a6ed16c21573cabe33f2a2aca8564
behailuG <barch.mami@gmail.com>
2023-02-21 15:07:33 -0800

    multiple of 3 and 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-	int i, sum=0;
+	int i, sum = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
-		if (((i % 3) == 0) || ((i % 5)==0))
+		if (((i % 3) == 0) || ((i % 5) == 0))
 		{
 			sum += i;
 		}
