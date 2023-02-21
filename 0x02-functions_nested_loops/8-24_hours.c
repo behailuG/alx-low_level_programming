9963a7f674609e76fb9f1e84d195e94f906e8640
behailuG <barch.mami@gmail.com>
2023-02-21 11:05:41 -0800

    jack baure

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * jack_bauer - jack bauer watch
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i == 2 && j ==4)
+				break;
+			int k, l;
+			for (k = 0; k < 6; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');	
+				}
+			}
+		}
+	}
+}
