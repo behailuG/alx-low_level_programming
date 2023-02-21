0a77df6a68c0edd3175f99b476b36d2c53306cf6
behailuG <barch.mami@gmail.com>
2023-02-21 07:20:55 -0800

    number sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include "main.h>"
+/**
+ * print_sign -> prints sign of number
+ * @n: parameter
+ * Return: + for positive, 0 for zero and - for negative
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+		return (+);
+	else if (n == 0)
+		return (0);
+	else
+		return (-);
+}
