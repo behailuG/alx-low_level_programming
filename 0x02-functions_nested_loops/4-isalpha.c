1efe6f76d72b0a8a0150341dd76185083f483f81
behailuG <barch.mami@gmail.com>
2023-02-21 07:09:46 -0800

    is alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha - is alphabet
+ * @c: parameter
+ * Return: Always 1 for alphabet else 0
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z) || (c >= 'A' && c <= 'Z') ? 1 : 0);
+}
