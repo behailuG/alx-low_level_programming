6d60652c716bf54b863ed06c9f2f1d38bf6a34f8
behailuG <barch.mami@gmail.com>
2023-02-21 14:41:47 -0800

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,60 +5,36 @@
  */
 void print_to_98(int n)
 {
-	int i, rem, k, nn, len = 0, number;
-	char str[5]="abcde";
-    number = n;
-    nn = n;
-    while (number != 0)
-    {
-        len++;
-        number /= 10;
-    }
-    for (i = 0; i < len; i++)
-    {
-        rem = n % 10;
-        n = n / 10;
-        str[len - (i + 1)] = rem + '0';
-    }
-    str[len] = '\0';
-	if (nn <= 98)
+	if (n <= 98)
 	{
-    		for (; nn <= 98; nn++)
-    		{
-    			for (k = 0; k < len; k++)
-    			{
-		 		_putchar(str[k]);
-    			}
-			if (nn != 98)
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
 			{
-				_putchar(',');
-				_putchar(' ');
+				printf("%d", n);
+				printf("\n");
+				break;
 			}
 			else
 			{
-				_putchar('\n');
-				break;
+				printf("%d, ", n);
 			}
-    		}
+		}
 	}
 	else
 	{
-		for (; nn >= 98; nn--)
+		for (; n >= 98; n--)
 		{
-			for (k = 0; k < len; k++)
+			if (n == 98)
 			{
-				_putchar(str[k]);
-			}
-			if (nn != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
+				printf("%d", n);
+				printf("\n");
+				break;
 			}
 			else
 			{
-				_putchar('\n');
-				break;
+				printf("%d, ", n);
 			}
-		}	
+		}
 	}
 }
