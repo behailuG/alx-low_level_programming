fa741333b1efb8ea60fa130bb726444545d1dcf3
behailuG <barch.mami@gmail.com>
2023-02-21 05:23:29 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,6 +8,14 @@
 int main(void)
 {
     print_alphabet();
-    _putchar('\n');
     return (0);
+}
+void print_alphabet()
+{
+	char lower;
+	for (lewer = 'a'; lower <= 'z'; lower++)
+	{
+		_putchar(lower);
+	}
+	_putchar('\n');
 }
