01fb476c129993ef1b41d81b8a9ca817dcaaf0e6
behailuG <barch.mami@gmail.com>
2023-02-23 11:10:51 -0800

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,10 @@
 #include "main.h"
 /**
+ * _isupper - upercase
+ * @c: parameter
+ * Return: 1
  */
 int _isupper(int c)
 {
-	return ((isupper(c + '0') ? 1 : 0);
+	return (isupper(c + '0') ? 1 : 0);
 }
