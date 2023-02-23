469e087dfaeab260a02c59e6293e622d02d3dd60
behailuG <barch.mami@gmail.com>
2023-02-23 12:39:16 -0800

    print 10 times number

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,17 @@ void more_numbers(void)
 	int i, j;
 
 	i = 0;
-	while (i <= 10)
+	while (i < 10)
 	{
 		j = 0;
 		while (j < 15)
 		{
-			_putchar(j + '0');
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+
+			_putchar((j % 10) + '0');
 			j++;
 		}
 		_putchar('\n');
