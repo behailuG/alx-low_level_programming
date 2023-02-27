a7e01a8986f5977e9d95854bf9e3552e43b92785
behailuG <barch.mami@gmail.com>
2023-02-27 12:17:41 -0800

    display string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,13 +5,9 @@
  */
 void _puts(char *str)
 {
-	int i;
-
-	i = 0;
-	while (str[i] != '\0')
+	for (; *str != '\0'; str++)
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(*str);
 	}
 	_putchar('\n');
 }
