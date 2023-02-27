fbbcf64abf2f170e67504df991ec6a916a283fb2
behailuG <barch.mami@gmail.com>
2023-02-27 13:26:25 -0800

    half display

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,13 +5,16 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i, j;
 
-	i = 0;
-	while (str[i] != '\0')
+	j = 0;
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i < j; i += 2)
 	{
 		_putchar(str[i]);
-		i += 2;
 	}
 	_putchar('\n');
 }
