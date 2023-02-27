fafb981d2b337241a32c4ac138d2d5103b13ea6c
behailuG <barch.mami@gmail.com>
2023-02-27 12:56:26 -0800

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - revese string
+ * @s: param
+ */
+void rev_string(char *s)
+{
+	int counter, i;
+	
+	char *a, b;
+
+	counter = 0;
+	while (s[counter] != '\0')
+		counter++;
+	a = *(a + (counter - 1));
+	for (i = 0; i < (counter / 2); i++)
+	{
+		b = s[i];
+		s[i] = *a;
+		*a = b;
+		a--;
+	}
+}
