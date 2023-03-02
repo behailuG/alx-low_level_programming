6741db96ece36c42cdd199835c5be5b16b388429
behailuG <barch.mami@gmail.com>
2023-03-02 07:54:29 -0800

    string concatination

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * *_strcat - string concatination
+ * @dest: destination
+ * @src: source
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (*dest + *src);
+}
