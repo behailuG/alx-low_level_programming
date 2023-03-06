f708c68faa21a4bff51fddf399c4104cdbed8418
behailuG <barch.mami@gmail.com>
2023-03-06 05:09:49 -0800

    string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,16 +7,17 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int i = 0, j = 0;
 
 	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-		{
-			s += i;
-			return (s);
-		}
 		i++;
+	for (j = 0; j <= a; j++)
+	{
+		if (s[j] == c)
+		{
+			s += j;
+			return (s);
+		
 	}
 	return ('\0');
 }
