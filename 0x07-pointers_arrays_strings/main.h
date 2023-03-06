0e2b9539ee8d19b47eb447dc823c8ca287aef728
behailuG <barch.mami@gmail.com>
2023-03-06 08:07:12 -0800

    set pointer

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,3 +7,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 char _putchar(char a);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
