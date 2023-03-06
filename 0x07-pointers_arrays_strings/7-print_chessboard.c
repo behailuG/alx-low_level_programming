5509ca8e17e8bd5ccc1fff4400d9f44d79087c89
behailuG <barch.mami@gmail.com>
2023-03-06 06:32:27 -0800

    two dimensional array

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -13,11 +13,6 @@ void print_chessboard(char (*a)[8])
 		while (j < 8)
 		{
 			_putchar(a[i][j]);
-			if (j != 7)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
 			j++;
 		}
 		_putchar('\n');
