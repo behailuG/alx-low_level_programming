82f835616e52b78998e739bbd9e1f9325b3abfff
behailuG <barch.mami@gmail.com>
2023-03-08 11:43:29 -0800

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - factorial calc
+ * @n: numbber of fictorial
+ * Retutn: factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1);
+}
