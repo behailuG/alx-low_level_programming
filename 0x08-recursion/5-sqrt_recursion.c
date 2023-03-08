46a53af5c13f82ac2d4d303cfdc95a4fc44d20dd
behailuG <barch.mami@gmail.com>
2023-03-08 12:51:13 -0800

    square root of number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,25 @@
 #include "main.h"
-/**
- * _sqrt - square root checker
- * @i: number
- * _sqrt_recursion - square of what
- * @n: square root
- * Return: number
- */
-int _sqrt(int n, int i);
 
+int _sqrt(int n, int i);
+/**
+ * _sqrt_recursion - square root
+ * @n: number
+ * Return: square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+/**
+ * _sqrt - square root finder
+ * @n: root
+ * @i: number
+ * Return: square
+ */
 int _sqrt(int n, int i)
 {
 	if ((i * i) > n)
 		return (-1);
 	else
 		return ((i * i) == n ? i : _sqrt(n, i + 1));
-}
-
-int _sqrt_recursion(int n)
-{
-	return (_sqrt(n, 1));
 }
