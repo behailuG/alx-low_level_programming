95ebdcd8bb2393c5d43f41a55e92eddf3232035f
behailuG <barch.mami@gmail.com>
2023-03-08 09:55:36 -0800

    string recursion display

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - display string recursivly
+ * @s: string
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	_putchar(s[i]);
+	s++;
+	if (s[i])
+		_puts_recursion(s);
+	_putchar('\n');
+}
