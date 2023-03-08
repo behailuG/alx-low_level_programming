2f3349e445648ef384e80a24d1e99debe8f6e453
behailuG <barch.mami@gmail.com>
2023-03-08 11:04:23 -0800

    string display reversly

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,18 +7,14 @@ void _print_rev_recursion(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
-	{
-		s++;
-		i++;
-	}
-	i = 0;
 	if (s[i] != 0)
 	{
-		_putchar(*s);
-		s--;
-		_print_rev_recursion(s);
+		if (s[i] != '\0')
+		{
+			s++;
+			_print_rev_recursion(s);
+		}
 	}
-	if (s[i] == '\0')
-		_putchar('\n');
+	s--;
+	_putchar(*s);
 }
