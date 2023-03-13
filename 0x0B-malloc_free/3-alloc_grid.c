851bbb638a814219f478cd2d4dcca2975280567e
behailuG <barch.mami@gmail.com>
2023-03-13 08:30:27 -0700

    multi diamentional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,6 +19,18 @@ int **alloc_grid(int width, int height)
 	{
 		for (i = 0; i < height; i++)
 		{
+			s[i] = malloc(sizeof(int) * width);
+
+			if (s[i] == NULL)
+			{
+				for (j = i; j >= 0; j--)
+				{
+					free(s[j]);
+				}
+
+				free(s);
+				return (NULL);
+			}
 			for (j = 0; j < width; j++)
 			{
 				s[i][j] = 0;
