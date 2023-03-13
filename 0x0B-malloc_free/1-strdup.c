72e9bdac09e55f61c577db7cb3a589f8073d7d48
behailuG <barch.mami@gmail.com>
2023-03-13 05:45:44 -0700

    new memory allocation

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *_strdup - new memory allocation
+ * @str: string
+ * Return: new pointer
+ */
+char *_strdup(char *str)
+{
+	int i = 0, j = 0;
+
+	char *s;
+
+	if (str != NULL)
+	{
+		while (str[j] != '\0')
+			j++;
+		while (i < j)
+		{
+			s[i] = str[i];
+			i++;
+		}
+		return (s);
+	}
+	return (NULL);
+}
