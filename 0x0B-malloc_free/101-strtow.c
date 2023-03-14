64b3309a305cae4344f1108ef433ef095fc4729d
behailuG <barch.mami@gmail.com>
2023-03-14 02:22:23 -0700

    string substring

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,25 +1,78 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * len - length of word
+ * @str: string
+ * Return: length of word
+ */
+int len(char *str)
+{
+	int i = 0;
+
+	while (str[i] != 32)
+		i++;
+	return (i);
+}
+/**
+ * wcount - word count
+ * @str: string
+ * Return: word
+ */
+int wcount(char *str)
+{
+	int i = 0, j = 0, word = 0;
+
+	while (str[i] != '\0')
+		i++;
+	for (; j < i; j++)
+	{
+		if (str[j] != 32)
+		{
+			word++;
+			j += len(str + j);
+		}
+	}
+	return (word);
+}
+
+/**
  * **strtow - string substring
  * @str: string
  * Return: return string
  */
 char **strtow(char *str)
 {
-	int i;
-	char *s;
+	int i, w, words = 0, l, letters = 0;
+	char **s;
 
-	if (str == NULL)
+	if (str == NULL || str[0] != '\0')
 		return (NULL);
-	i = 0;
-	while (str[i] != '\0')
+	words = wcount(str);
+	if (words > 0)
 	{
-		if (str[i] == 9 || str[i] == 32)
-			str[i] = '$';
-		s[i] = str[i];
-		i++;
+		s = malloc((sizeof(char *) * words) + 1);
+		if (s != NULL)
+		{
+			for (w = 0; w < words; w++)
+			{
+				while (str[i] == 32)
+					i++;
+				letters = len(str + i);
+				s[w] = malloc((sizeof(char *) * letters) + 1);
+				if (s[w] != NULL)
+				{
+					for (l = 0; l < letters; l++)
+						s[w][l] = str[i++];
+					s[w][l] = '\0';
+				}
+				else
+				{
+					free(s[w]);
+					free(s);
+				}
+			}
+			return (s);
+		}
 	}
-
-	return (s);
+	return (NULL);
 }
