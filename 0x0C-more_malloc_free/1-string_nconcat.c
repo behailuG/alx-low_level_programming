5282511f7f06eaad860063ecb47ed4a2cb6ad2cd
behailuG <barch.mami@gmail.com>
2023-03-16 06:06:46 -0700

    string n concatination

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,16 +9,16 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i = 0, j = 0, k, l = 0;
+	int i = 0, j = 0, k = 0, l = 0;
 	char *s;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[i] != '\0')
+	while (s1[i])
 		i++;
-	while (s2[j] != '\0')
+	while (s2[j])
 		j++;
 	if (n >= j)
 		k = i + j;
