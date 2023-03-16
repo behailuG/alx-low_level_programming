dddbf767517fa1b9f53f9b568cfeec5eed3d1ed0
behailuG <barch.mami@gmail.com>
2023-03-16 06:36:10 -0700

    allocate memory as calloc using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	i = nmemb * size;
-	s = malloc();
+	s = malloc(i);
 	if (s == NULL)
 		return (NULL);
 	while (j < i)
