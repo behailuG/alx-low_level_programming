080352c748967af0005dd64931cc0c9cc07bf8cd
behailuG <barch.mami@gmail.com>
2023-03-16 07:51:43 -0700

    multiple of two positive numbers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,36 @@
+#include <stdlib>
+#include <stdio.h>
+/**
+ * main - main method
+ * @argc: number of argument
+ * @argv: argument value
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	unsigned long mul = 1;
+	int i = 1, j;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (argv[i][j] < 48 && argv[i][j] > 57)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+			j++;
+		}
+		mul *= argv[i];
+		i++;
+	}
+	printf("%lu\n", mul);
+	return (0);
+}
