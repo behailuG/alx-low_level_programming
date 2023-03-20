226b383bbd963d0bc908847402a8109d4cca966f
behailuG <barch.mami@gmail.com>
2023-03-20 23:18:03 +0300

    print struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,12 @@
-#ifndef BEHAILU
-#define BEHAILU
+#ifndef HEADER
+#define HEADER
 
 /**
   * struct dog - Dog attributes
-  * @name: name.
-  * @age: age.
-  * @owner: owner.
+  * @name: The name of the dog.
+  * @age: The age of the dog.
+  * @owner: The owner of the dog.
+  *
   * Description: The attributes of a dog.
   */
 struct dog
@@ -15,8 +16,13 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dog_two;
+/**
+ * dog_t - Typedef for dog structure
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
