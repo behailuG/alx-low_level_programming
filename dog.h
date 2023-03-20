32dcedef0f3bb940f8af7d42d798a30797c90c96
behailuG <barch.mami@gmail.com>
2023-03-20 22:38:42 +0300

    structure type

---
+++ dog.h
@@ -0,0 +1,17 @@
+#ifndef BEHAILU
+#define BEHAILU
+
+/**
+  * struct dog - Dog attributes
+  * @name: name.
+  * @age: age.
+  * @owner: owner.
+  * Description: The attributes of a dog.
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
