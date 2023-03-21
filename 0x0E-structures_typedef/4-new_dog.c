396af00dac23594d67f0a6569adac6976c4b9a2b
Behailu Girma <120037990+behailuG@users.noreply.github.com>
2023-03-21 16:43:22 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,75 +5,62 @@ int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 
 /**
-  * new_dog - ...
-  * @name: ...
-  * @age: ...
-  * @owner: ...
-  *
-  * Return: ...
+  * new_dog - create new dog
+  * @name: name of new dog
+  * @age: age of new dog
+  * @owner: owner of the new dog
+  * Return: new dog containing pointer
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *cutie_dog;
-	int name_l = 0, own_l = 0;
+	dog_t *new_dog;
+	int name_len = 0, own_len = 0;
 
 	if (name != NULL && owner != NULL)
 	{
-		name_l = _strlen(name) + 1;
-		own_l = _strlen(owner) + 1;
-		cutie_dog = malloc(sizeof(dog_t));
-
-		if (cutie_dog == NULL)
+		name_len = _strlen(name) + 1;
+		own_len = _strlen(owner) + 1;
+		new_dog = malloc(sizeof(dog_t));
+		if (new_dog == NULL)
 			return (NULL);
-
-		cutie_dog->name = malloc(sizeof(char) * name_l);
-
-		if (cutie_dog->name == NULL)
+		(*new_dog).name = malloc(sizeof(char) * name_len);
+		if ((*new_dog).name == NULL)
 		{
-			free(cutie_dog);
+			free(new_dog);
 			return (NULL);
 		}
-
-		cutie_dog->owner = malloc(sizeof(char) * own_l);
-
-		if (cutie_dog->owner == NULL)
+		(*new_dog).owner = malloc(sizeof(char) * own_len);
+		if ((*new_dog).owner == NULL)
 		{
-			free(cutie_dog->name);
-			free(cutie_dog);
+			free((*new_dog).name);
+			free(new_dog);
 			return (NULL);
 		}
-
-		cutie_dog->name = _strcpy(cutie_dog->name, name);
-		cutie_dog->owner = _strcpy(cutie_dog->owner, owner);
-		cutie_dog->age = age;
+		(*new_dog).name = _strcpy((*new_dog).name, name);
+		(*new_dog).owner = _strcpy((*new_dog).owner, owner);
+		(*new_dog).age = age;
 	}
-
-	return (cutie_dog);
+	return (new_dog);
 }
 
 /**
   * _strlen - Returns the length of a string
   * @s: String to count
-  *
   * Return: String length
   */
 int _strlen(char *s)
 {
-	int c = 0;
+	int i = 0;
 
-	for (; *s != '\0'; s++)
-	{
-		c++;
-	}
-
-	return (c);
+	for (; *s != '\0'; i++)
+		i++;
+	return (i);
 }
 
 /**
   * _strcpy - Copy a string
   * @dest: Destination value
   * @src: Source value
-  *
   * Return: the pointer to dest
   */
 char *_strcpy(char *dest, char *src)
@@ -81,11 +68,7 @@ char *_strcpy(char *dest, char *src)
 	int i;
 
 	for (i = 0; src[i] != '\0'; i++)
-	{
 		dest[i] = src[i];
-	}
-
 	dest[i++] = '\0';
-
 	return (dest);
 }
