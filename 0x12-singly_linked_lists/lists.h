ae1567918e81d507426ea14d4454a68beb744bab
behailuG <barch.mami@gmail.com>
2023-03-30 11:12:06 -0700

    print all list length and content

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,18 @@
-#include <stdlib.h>
-#include <string.h>
+#ifndef LINKED_LIST
+#define LINKED_LIST
 #include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct list_s - singly linked list
+ * @stng: string
+ * @len: length of the string
+ * @next: points to the next node
+ */
+typedef struct list_s
+{
+	char *stng;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 size_t print_list(const list_t *h);
+#endif
