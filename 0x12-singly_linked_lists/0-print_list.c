9e5b42625fa3e0cf12b845221907ff114484747e
behailuG <barch.mami@gmail.com>
2023-03-30 10:54:39 -0700

    print all list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_list - print all list
+ * @h: list
+ * Return: node list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t node;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		node++;
+	}
+	return (node);
+}
---
+++ 0x12-singly_linked_lists/main.h
@@ -0,0 +1,4 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+size_t print_list(const list_t *h);
