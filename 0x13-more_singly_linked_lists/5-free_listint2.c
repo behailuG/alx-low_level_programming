29e4ed117a5389d9c3c28c3c958d86c4ce5f31e8
behailuG <barch.mami@gmail.com>
2023-04-03 09:08:38 -0700

    free lists of pointer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint2 - free list
+ * @head: node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	if (head != NULL)
+	{
+		while (*head)
+		{
+			node = (*head);
+			*head = (*head)->next;
+			free(node);
+		}
+	}
+	else
+		return;
+	free(*head);
+}
