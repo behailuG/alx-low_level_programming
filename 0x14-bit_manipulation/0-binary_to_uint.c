33452d32fd4a8e178ce1875d4aea6b6a21d31bac
behailuG <barch.mami@gmail.com>
2023-04-06 12:52:14 -0700

    convert binary to unit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * binary_to_uint - convert binary to unit
+ * @b: binary
+ * Return: unit
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0;
+	int len = 0, sum = 0;
+
+	if (b == NULL)
+		return (0);
+	len = checkBinary(b);
+	if (len > 0)
+	{
+		while (len > 0)
+		{
+			if (b[len] == 49)
+				sum += 1 << i;
+			i++;
+			len--;
+		}
+	}
+	return (sum);
+}
+/**
+ * checkBinary - check the given string is binary
+ * @binary: binary number
+ * Return: legth;
+ */
+int checkBinary(const char *binary)
+{
+	unsigned int i = 0;
+
+	while (binary[i])
+	{
+		if (binary[i] != 48 && binary[i] != 49)
+		{
+			i = 0;
+			break;
+		}
+		i++;
+	}
+	return (i);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,7 @@
+#ifndef HEADER
+#define HEADER
+#include <stdio.h>
+#include <stdlib.h>
+int checkBinary(const char *binary);
+unsigned int binary_to_uint(const char *b);
+#endif
