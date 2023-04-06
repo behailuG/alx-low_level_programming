bb15f67db00393e1333660dc6170887ad32ba482
behailuG <barch.mami@gmail.com>
2023-04-06 13:14:24 -0700

    binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,20 +6,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = 0;
-	int len = 0, sum = 0;
+	unsigned int sum = 0;
+	int len = 0, c = 1;
 
 	if (b == NULL)
 		return (0);
 	len = checkBinary(b);
 	if (len > 0)
 	{
-		while (len > 0)
+		while (len--)
 		{
 			if (b[len] == 49)
-				sum += 1 << i;
-			i++;
-			len--;
+				sum += c;
+			c *= 2;
 		}
 	}
 	return (sum);
