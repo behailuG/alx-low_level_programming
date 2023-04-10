52161875681109bd008e568a28681d5faf84a671
behailuG <barch.mami@gmail.com>
2023-04-10 13:32:19 -0700

    copy to another

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,4 +1,4 @@
-#ifdef HEADER
+#ifndef HEADER
 #define HEADER
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -10,4 +10,5 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void copy_file(const char *src, const char *dest);
 #endif
