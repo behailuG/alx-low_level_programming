0eb44e001f13ef4e1c3449b46060a4b93ad126a1
behailuG <barch.mami@gmail.com>
2023-04-10 12:54:32 -0700

    header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,10 @@
 #ifdef HEADER
 #define HEADER
-#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
